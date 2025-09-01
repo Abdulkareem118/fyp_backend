c7f9e38af7f02e1ed54e4d59fdb760c4a6c07e01
Abdulkareem118 <swabdulkareem@gmail.com>
2025-09-01 12:04:36 +0500

    changes

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const cors = require("cors");
 
 const app = express();
 const PORT = 5000;
+app.use(
+  cors({
+    origin: ["https://collabortive-project.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true, 
+  })
+);
 
 app.use(cors());
 app.use(express.json());
